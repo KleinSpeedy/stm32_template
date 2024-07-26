eac14e42de3b6c801efd43b2141c6afa874795ad
Jonas Schulze <j.schulze@senec.com>
2024-07-26 11:58:23 +0200

    Add hal configuration and simple gpio test project

--- src/main.c
+++ src/main.c
@@ -1,8 +1,31 @@
 #include "bool.h"
+#include "stm32f429xx.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
 
 int main(void)
 {
-    while(TRUE);
+    HAL_Init();
+
+    // configuration of port
+    GPIO_InitTypeDef gpioInitA = {
+        .Pin = GPIO_PIN_1,
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_PULLUP,
+        .Speed = GPIO_SPEED_FAST
+    };
+
+    HAL_GPIO_Init(GPIOA, &gpioInitA);
+
+    // set pin to low
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+
+    while(TRUE)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
+        // Delay 1s
+        HAL_Delay(1000);
+    }
 
     return 0;
 }
