297e366cc85b3f356239ffbed33d7d3db9b75ad5
Jonas Schulze <j.schulze@senec.com>
2024-08-09 13:31:54 +0200

    WIP: Add real vector table from auto generated startup code

--- startup/crt1.c
+++ startup/crt1.c
@@ -2,8 +2,18 @@
  * C function definitions for assembly startup
  */
 
+
+/*
+ * Default exit handler, called after main returns, runs in endless loop
+ */
 __attribute__((noreturn, weak))
 void __exit(void)
+{
+    while(1);
+}
+
+__attribute__((noreturn, weak))
+void Default_Handler(void)
 {
     while(1);
 }
