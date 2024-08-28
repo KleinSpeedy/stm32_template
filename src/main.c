ae3a018cf598b3b2ec958f0397969051d4fddeaa
Jonas Schulze <j.schulze@senec.com>
2024-08-28 08:39:07 +0200

    Add gpio locking as test case

--- src/main.c
+++ src/main.c
@@ -2,22 +2,43 @@
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_gpio.h"
 
+typedef enum ErrorReason{
+    NO_ERR = 0,
+    LOCK_ERR,
+
+    NUM_ERR
+}ErrorReason;
+
+static void error_loop(enum ErrorReason err)
+{
+    (void)err;
+    while(1)
+    {
+    }
+}
+
 int main(void)
 {
     HAL_Init();
 
-    // configuration of port
+    // configuration of port A pin 1
     GPIO_InitTypeDef gpioInitA = {
         .Pin = GPIO_PIN_1,
         .Mode = GPIO_MODE_OUTPUT_PP,
         .Pull = GPIO_PULLUP,
-        .Speed = GPIO_SPEED_FAST
+        .Speed = GPIO_SPEED_LOW
     };
 
     HAL_GPIO_Init(GPIOA, &gpioInitA);
 
-    // set pin to low
+    // set pins to low
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+
+    // lock all pins to current configuration
+    if(HAL_GPIO_LockPin(GPIOA, GPIO_PIN_All) != HAL_OK)
+    {
+        error_loop(LOCK_ERR);
+    }
 
     while(TRUE)
     {
